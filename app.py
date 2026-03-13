6a90794c67dbc459857236b2480edc82a466271f
Alejandro Meza <elmeza58@gmail.com>
2026-03-13 14:00:06 -0700

    Webhook7.3

--- app.py
+++ app.py
@@ -375,7 +375,7 @@ def enviar_mensajes_whatsapp(texto,number):
     connection = http.client.HTTPSConnection("graph.facebook.com")
 
     try:
-        connection.request("POST","/v22.0/1077626325424911/messages", data, headers)
+        connection.request("POST","/v18.0/117721278011867/messages", data, headers)
         response = connection.getresponse()
         print(response.status, response.reason)
     except Exception as e:
